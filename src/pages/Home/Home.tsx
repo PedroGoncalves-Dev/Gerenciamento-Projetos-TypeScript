d61293911104f1c6fc8636b469d08837903e35e4
Pedro <pedro_developer@outlook.com>
2024-09-29 02:01:15 -0300

    instalando tailwind shadcn

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,7 +4,7 @@ import styles from "./Home.module.css";
 const Home = () => {
   return (
     <section>
-      <h1>pesquisa...</h1>
+      <h1 className="#334155">pesquisa...</h1>
       <GridProjetoUsuario />
       <h3>mais conteudos a colocar</h3>
     </section>
