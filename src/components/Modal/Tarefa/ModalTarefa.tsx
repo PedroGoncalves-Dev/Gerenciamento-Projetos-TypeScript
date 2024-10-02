5cd4da202a36b1c8e222e2bbdc2106f8bc9e682c
Pedro <pedro_developer@outlook.com>
2024-10-01 23:54:06 -0300

    estilizando cardProjeto

--- src/components/Modal/Tarefa/ModalTarefa.tsx
+++ src/components/Modal/Tarefa/ModalTarefa.tsx
@@ -35,7 +35,7 @@ const ModalTarefa = ({ id_projeto }: Iprops) => {
 
   const fetchTarefa = async () => {
     const res = await api.get<IdadosT>(urlTarefa);
-    return console.log(id_projeto), res.data;
+    return res.data;
   };
 
   const {
@@ -67,7 +67,7 @@ const ModalTarefa = ({ id_projeto }: Iprops) => {
   if (error) return <div>'erro</div>;
   if (isLoading) return <div>'carregando'</div>;
 
-  if (erroDetalhesTarefa) return <p>{erroDetalhesTarefa.message}</p>;
+  if (erroDetalhesTarefa) return <p>Tarefas não econstradas</p>;
   if (loadingDetalhesTarefa) return <p>carregando...</p>;
 
   return (
