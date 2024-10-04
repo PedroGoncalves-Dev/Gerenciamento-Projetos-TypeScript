7e7396e7d78c6cc5cf64520fa43ac6b6e82b7ddc
Pedro <pedro_developer@outlook.com>
2024-10-04 01:05:32 -0300

    criando função de login

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,17 +1,40 @@
 import styles from "./Login.module.css";
 import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+interface IpropsSubmit {
+  username_usu: string;
+  senha_usu: string;
+}
 
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IpropsSubmit>();
+
+  const onSubmit = (data: IpropsSubmit) => {};
   return (
-    <form className={styles.formL}>
+    <form className={styles.formL} onSubmit={handleSubmit(onSubmit)}>
       <label>
         <span>UserName:</span>
-        <input type="text" placeholder="Seu userName para poder acessar" />
+        <input
+          type="text"
+          placeholder="Seu userName para poder acessar"
+          {...register("username_usu", { required: "Username obrigatorio" })}
+        />
+        {errors.username_usu && <p>{errors.senha_usu?.message}</p>}
       </label>
 
       <label>
         <span>Senha:</span>
-        <input type="password" placeholder="****" />
+        <input
+          type="password"
+          placeholder="****"
+          {...register("senha_usu", { required: "Senha obrigatoria" })}
+        />
+        {errors.senha_usu && <p>{errors.senha_usu.message}</p>}
       </label>
 
       <button type="submit">Login</button>
